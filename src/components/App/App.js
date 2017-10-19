5d06d328ca6b3df4bb0fc93eb4c167dca6612f66
tbishop95 <tom@bso.co.uk>
2017-10-19 07:24:11 +0100

    Update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import SearchResults from './components/SearchResults/SearchResults';
+import Playlist from './components/Playlist/Playlist';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [ 
+        {name: 'Did You See', artist: 'J Hus', album: 'Common Sense'},
+        {name: 'Glory Days', artist: 'Just Jack', album: 'Overtones'},
+        {name: 'Re-Wired', artist: 'Kasabian', album: 'Velociraptor'}
+      ],
+      playlistName: 'My list',
+      playlistTracks:
+      [ {name: 'Biblical', artist: 'Biffy Clyro', album: 'Opposites (Deluxe)'},
+        {name: 'Hook', artist: 'Blues Traveler', album: 'Cover Yourself'},
+        {name: 'Magic', artist: 'Coldplay', album: 'Ghost Stories'}
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
@@ -10,8 +32,8 @@ class App extends Component {
   <div className="App">
     <SearchBar />
     <div className="App-playlist">
-      <SearchResults />
-      <Playlist />
+      <SearchResults searchResults = {this.state.searchResults}  />
+      <Playlist playlistName = {this.state.playlistName} playlistTracks = {this.state.playlistTracks}/>
     </div>
   </div>
 </div>
--- src/components/TrackList/Tracklist.js
+++ src/components/TrackList/Tracklist.js
@@ -8,6 +8,11 @@ class TrackList extends Component {
     return (
     	<div className="TrackList">
     <!-- You will add a map method that renders a set of Track components  -->
+    { this.props.tracks.map(track => {
+          return (
+            <Track track={ track } key={track.id} />
+          );
+        }) }
 </div>
     );
   }
